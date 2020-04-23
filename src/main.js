e836fb0528d25fbea4dbb8bfee6efcf2a45cfd49
Daniel Merys <dkmerys@gmail.com>
2020-04-23 09:27:30 -0700

    add js file for gif reaction and update front-end js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
 import { DadJoke } from './../src/dad-joke.js';
+import { ReactionGif } from './../src/gif-reaction.js';
 
 $(document).ready(function () {
   $('#makeJoke').click(function () {
@@ -11,8 +12,11 @@ $(document).ready(function () {
 
     (async () => {
       let dadJoke = new DadJoke();
+      let reactionGif = new ReactionGif();
       const response = await dadJoke.getRandomDadJoke();
+      const gifResponse = await reactionGif.getRandomGif();
       console.log(response);
+      console.log(gifResponse);
       getElements(response);
     })();
 
@@ -21,6 +25,14 @@ $(document).ready(function () {
         $('.showJoke').text(`${response.joke}`);
       } else {
         $('.showJoke').text("Sorry, we could not get your joke. You probably wouldn't get it anyway.");
+      }
+    }
+
+    function getGifElements(gifResponse) {
+      if (gifResponse) {
+        $('.showGif').html(`<img src="${gifResponse.data[0].embed_url}" alt="random image of someone groaning>`)
+      } else {
+        $('showGif').text("Sorry, we could not get a gif.");
       }
     }
   });
